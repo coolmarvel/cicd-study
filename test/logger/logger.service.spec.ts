88118ac71b34e3b2fad18acea25dbd534680966d
coolmarvel <marvel97@naver.com>
2024-03-06 09:35:49 +0900

    test coverage add & ci test

---
+++ test/logger/logger.service.spec.ts
@@ -0,0 +1,12 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { LoggerService } from 'src/common/logger/service/logger.service';
+
+describe('LoggerService', () => {
+  let service: LoggerService;
+
+  beforeEach(async () => {});
+
+  it('should be defined', () => {
+    console.log('should be success');
+  });
+});
