4b30d7b0c6db64a53f329bcd2e02b791642ebc14
coolmarvel <marvel97@naver.com>
2024-03-07 11:38:40 +0900

    mongo replSet in docker setting complete

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -4,10 +4,10 @@ export default registerAs(
   'database',
   (): Record<string, any> => ({
     host: process.env?.DATABASE_HOST ?? 'mongodb://localhost:27017,localhost:27018,localhost:27019',
-    name: process.env?.DATABASE_NAME ?? 'ack',
+    name: process.env?.DATABASE_NAME ?? 'boilerplate',
     user: process.env?.DATABASE_USER,
     password: process?.env.DATABASE_PASSWORD,
-    debug: process.env.DATABASE_DEBUG === 'true',
+    debug: Boolean(process.env.DATABASE_DEBUG) === true,
     options: process.env?.DATABASE_OPTIONS,
     timeoutOptions: {
       serverSelectionTimeoutMS: 10000,
