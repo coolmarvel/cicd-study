96c3093ce723c495334f32596b6e9d2fefcb3fb3
coolmarvel <marvel97@naver.com>
2024-03-08 17:13:24 +0900

    router module generate and apply to app module

---
+++ src/router/router.module.ts
@@ -0,0 +1,33 @@
+import { DynamicModule, ForwardReference, Module, Type } from '@nestjs/common';
+import { RouterModule as NestJsRouterModule } from '@nestjs/core';
+import { RoutesAdminModule } from './routes/routes.admin.module';
+import { RoutesUserModule } from 'src/router/routes/routes.user.module';
+import { RoutesPublicModule } from 'src/router/routes/routes.public.module';
+import { RoutesAuthModule } from 'src/router/routes/routes.auth.module';
+import { RoutesServiceModule } from 'src/router/routes/routes.service.module';
+
+@Module({})
+export class RouterModule {
+  static forRoot(): DynamicModule {
+    const imports: (DynamicModule | Type<any> | Promise<DynamicModule> | ForwardReference<any>)[] = [];
+
+    if (Boolean(process.env.HTTP_ENABLE) === true) {
+      imports.push(
+        RoutesPublicModule,
+        RoutesUserModule,
+        RoutesAdminModule,
+        RoutesAuthModule,
+        RoutesServiceModule,
+        NestJsRouterModule.register([
+          { path: '/public', module: RoutesPublicModule },
+          { path: '/admin', module: RoutesAdminModule },
+          { path: '/user', module: RoutesUserModule },
+          { path: '/auth', module: RoutesAuthModule },
+          { path: '/service', module: RoutesServiceModule },
+        ]),
+      );
+    }
+
+    return { module: RouterModule, providers: [], exports: [], controllers: [], imports };
+  }
+}
