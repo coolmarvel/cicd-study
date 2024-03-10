49b5195d19235cc5a1f6c33f5ff5132ec1194b2d
coolmarvel <marvel97@naver.com>
2024-03-10 13:31:26 +0900

    schedule module install for jobs scheduling and generate module jobs and apply to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-// import { JobsModule } from 'src/jobs/jobs.module';
+import { JobsModule } from 'src/jobs/jobs.module';
 import { AppController } from './controllers/app.controller';
 import { RouterModule } from 'src/router/router.module';
 import { CommonModule } from 'src/common/common.module';
@@ -13,7 +13,7 @@ import { AppMiddlewareModule } from 'src/app/middleware/app.middleware.module';
     AppMiddlewareModule,
 
     // Jobs
-    // JobsModule.forRoot(),
+    JobsModule.forRoot(),
 
     // Routes
     RouterModule.forRoot(),
